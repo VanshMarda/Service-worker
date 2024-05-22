5f0e4ea78057d0d9777a249eb4de49826878902a
Vansh Marda <vansh.marda@vansh.marda-C02DV2MBMD6N>
2024-05-22 12:56:24 +0530

    removed-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
- 
-import withPWA from 'next-pwa';
-import runtimeCaching from 'next-pwa/cache.js';
-const isProduction = process.env.NODE_ENV === 'production';
- 
 const config = {
 };
  
-const nextConfig = withPWA({
-    dest: 'public',
-    cacheOnFrontEndNav:true,
-    reloadOnOnline:true,
-    disable:false,
-})(
-  config
-);
  
-export default nextConfig;
+export default config;
