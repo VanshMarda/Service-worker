b779125480618b0633fe4e1abd333d5edefdf710
Vansh Marda <vansh.marda@vansh.marda-C02DV2MBMD6N>
2024-05-23 09:38:11 +0530

    fixed respondWith

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,44 @@
-// self.addEventListener('install', (event) => {
-//     console.log('Service Worker installing.');
-//     // Perform any necessary setup
-//   });
-  
-//   self.addEventListener('activate', (event) => {
-//     console.log('Service Worker activating.');
-//     // Clean up old caches or other tasks
-//   });
-  
-self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
-    console.log('Fetch event for:', event.request.url);
-    const domain_name_1 = "i.ibb.co"
-    const domain_name_2 = "space-prod2.sprinklr.com/ui/rest"
-    // const path_name = "/50400/6e6122af-bf42-4d21-9f5c-6d5bea9e0b43-458261680/Image_CXWise_p.jpg"
-    // event.respondWith(fetch(event.request));
-    // if (url.hostname === domain_name_1 && url.pathname.startsWith('/CwMXtFm')) {
-    //   const newUrl = url.href.replace(domain_name_1, domain_name_2);
-    //   console.log("passed 1")
-    //   event.respondWith(fetch(newUrl));
-    // } 
-    // else {
-    //   console.log("passed 2")
-    //   event.respondWith(fetch(event.request));
-    // }
-  });
-
 // self.addEventListener('fetch', (event) => {
-//     console.log('Fetch event for:', event.request.url);
-  
-//     if (event.request.url.includes('Image_CXWise_p.jpg')) {
-//       console.log('Matched image URL:', event.request.url);
-      
-//       event.respondWith(
-//         fetch(event.request, { mode: 'cors' }).then((response) => {
-//           if (!response || response.status !== 200 || response.type !== 'basic') {
-//             console.error('Error fetching the image:', response);
-//             return response;
-//           }
-  
-//           console.log('Image response status:', response.status);
-//           return response;
-//         }).catch((error) => {
-//           console.error('Fetch error:', error);
-//           throw error;
-//         })
-//       );
-//     } else {
-//       event.respondWith(fetch(event.request));
-//     }
-//   });
-  
-  
+//   const url = new URL(event.request.url);
+//   // console.log({ url, eventurl: event.request.url })
+//   const domain_name_1 = "i.ibb.co"
+//   // console.log(url.pathname.startsWith('/CwMXtFm'))
+
+//   if (url.hostname === domain_name_1 && url.pathname.startsWith('/CwMXtFm')) {
+//     // const newUrl = url.href.replace(domain_name_1, domain_name_2);
+//     const new_url = "https://i.ibb.co/fNQ1CX1/android-chrome-384x384.png"
+//     // console.log("passed 1")
+//     // url.href = new_url
+//     // event.respondWith(fetch(new_url));
+//     event.request.url = new_url
+//     event.respondWith((async () => {
+//       const response = await fetch(event.request)
+//       const response1 = await fetch(url)
+//       console.log(response,response1)
+//       return response
+//     })())
+
+//   }
+//   else {
+//     event.respondWith(fetch(event.request));
+//   }
+// });
+
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  const domain_name_1 = "i.ibb.co";
+
+  if (url.hostname === domain_name_1 && url.pathname.startsWith('/CwMXtFm')) {
+    const new_url = "https://i.ibb.co/fNQ1CX1/android-chrome-384x384.png";
+    const newRequest = new Request(new_url, event.request);
+    event.respondWith((async () => {
+      const response = await fetch(newRequest);
+      console.log('Original Request URL:', event.request);
+      console.log('New Request URL:', newRequest);
+      return response;
+    })());
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+});
